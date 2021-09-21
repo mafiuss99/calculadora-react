20050411f7fb24fee23cc307e517b6d00a2c0820
Matheus Felipe <mf.mf98.23@gmail.com>
2021-09-21 16:40:26 -0300

    Criação de lógica da calculadora em React

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,13 @@ import './Button.css'
 
 export default props => {
     let classes = 'button'
-    classes += props.operation ? 'operation' : ''
-    classes += props.double ? 'double' : ''
-    classes += props.triple ? 'triple' : ''
+    classes += props.operation ? ' operation' : ''
+    classes += props.double ? ' double' : ''
+    classes += props.triple ? ' triple' : ''
 
     return (
         <button 
-            onClick={e => props.click && props.click(e.label)}
+            onClick={e => props.click && props.click(props.label)}
             className={classes}>
             {props.label}
         </button>
